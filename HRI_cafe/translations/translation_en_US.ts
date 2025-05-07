25f80f373aeceb21dcb461a056d91a857908c7d9
Yitong2 <z1259198239@gmail.com>
2025-05-07 11:28:52 +0100

    2025 .5. 7

---
+++ HRI_cafe/translations/translation_en_US.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="obsolete">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="obsolete">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="obsolete">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="obsolete">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK</translation>
+        </message>
+    </context>
+</TS>
